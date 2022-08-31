d0dc864c351b0e918f3d7f4160cb1cedddbd9618
owikle <owikle@users.noreply.github.com>
2022-08-31 23:04:10 +0000

    deploy: 369badc1ebfa37e5a04a710b402361bfcd738194

--- assets/js/lunr-store.js
+++ assets/js/lunr-store.js
@@ -1,1 +1,113 @@
-var store = [];
+var store = [{  
+"title": "Rock Music","date": "2008","creator": "Bukvich, Daniel","description": "Jazz Choir piece, can be played with or without percussion accompaniment.","location": "Moscow, ID", 
+"id": "bukvich0001"
+
+},{  
+"title": "Night Poem","date": "2003","creator": "Bukvich, Daniel","description": "Jazz band or combo piece. Scored for a flugelhorn soloist.","location": "Naperville, IL", 
+"id": "bukvich0002"
+
+},{  
+"title": "Tower Winds","date": "2010","creator": "Bukvich, Daniel","description": "Wind Ensemble","location": "Durham, NH", 
+"id": "bukvich0003"
+
+},{  
+"title": "Agincourt Hymn","date": "1986-01-26","creator": "Bukvich, Daniel","description": "Wind Ensemble","location": "Butte, MT", 
+"id": "bukvich0004"
+
+},{  
+"title": "Buffalo Jump Ritual","date": "2000-08-10","creator": "Bukvich, Daniel","description": "Concert Band","location": "Moscow, ID", 
+"id": "bukvich0005"
+
+},{  
+"title": "Flag Day Parade","creator": "Bukvich, Daniel","description": "Concert Band","location": "Moscow, ID", 
+"id": "bukvich0006"
+
+},{  
+"title": "March Prelude to the Star Spangled Banner","date": "2012","creator": "Bukvich, Daniel","description": "Concert Band","location": "Moscow, ID", 
+"id": "bukvich0007"
+
+},{  
+"title": "Hanukkah Shepherd's Song","creator": "Bukvich, Daniel","description": "String orchestra","location": "Durham, NH", 
+"id": "bukvich0008"
+
+},{  
+"title": "Three Native Songs","creator": "Bukvich, Daniel","description": "Concert band","location": "Weston, CT", 
+"id": "bukvich0009"
+
+},{  
+"title": "Five Fantasies on the Tones A & G for Brass Quintet","date": "2019","creator": "Bukvich, Daniel","description": "Brass quintet","location": "Moscow, ID", 
+"id": "bukvich0010"
+
+},{  
+"title": "Harmonie du soir","date": "2013-02-17","creator": "Bukvich, Daniel","description": "Solo violin and piano","location": "Moscow, ID", 
+"id": "bukvich0011"
+
+},{  
+"title": "Three and One-Half Songs for Alto Saxophone and Piano","date": "2020","creator": "Bukvich, Daniel","description": "Solo alto saxophone and piano","location": "Moscow, ID", 
+"id": "bukvich0012"
+
+},{  
+"title": "Sonata for Flute and Piano","date": "2005-04","creator": "Bukvich, Daniel","description": "Solo flute and piano","location": "Moscow, ID", 
+"id": "bukvich0013"
+
+},{  
+"title": "Three Anna Akhmatova Songs","date": "1996","creator": "Bukvich, Daniel","description": "Solo voice and piano","location": "Moscow, ID", 
+"id": "bukvich0014"
+
+},{  
+"title": "Sonata for Trumpet and Piano","date": "2008-04","creator": "Bukvich, Daniel","description": "Solo trumpet and piano","location": "Naperville, IL", 
+"id": "bukvich0015"
+
+},{  
+"title": "Light","date": "2019","creator": "Bukvich, Daniel","description": "Choir","location": "Moscow, ID", 
+"id": "bukvich0016"
+
+},{  
+"title": "Kinetic Code","date": "1990","creator": "Bukvich, Daniel","description": "Piano and percussion","location": "Moscow, ID", 
+"id": "bukvich0017"
+
+},{  
+"title": "Lullanova","creator": "Bukvich, Daniel","description": "Jazz combo","location": "Moscow, ID", 
+"id": "bukvich0018"
+
+},{  
+"title": "Mainspring","creator": "Bukvich, Daniel","description": "Jazz combo","location": "Moscow, ID", 
+"id": "bukvich0019"
+
+},{  
+"title": "Moving Violation","date": "2021","creator": "Bukvich, Daniel","description": "Jazz combo","location": "Moscow, ID", 
+"id": "bukvich0020"
+
+},{  
+"title": "In Memoriam Dresden","creator": "Bukvich, Daniel","description": "Wind ensemble","location": "Butte, MT", 
+"id": "bukvich0021"
+
+},{  
+"title": "Ballroom Portraits","creator": "Bukvich, Daniel","description": "Wind ensemble","location": "Moscow, ID", 
+"id": "bukvich0022"
+
+},{  
+"title": "Meditations on Writings by Vasily Kandinsky","date": "1996","creator": "Bukvich, Daniel","description": "Wind ensemble","location": "Moscow, ID", 
+"id": "bukvich0023"
+
+},{  
+"title": "Symphonic Movement","creator": "Bukvich, Daniel","description": "Wind ensemble","location": "Moscow, ID", 
+"id": "bukvich0024"
+
+},{  
+"title": "Dedication","creator": "Bukvich, Daniel","description": "Choir","location": "Moscow, ID", 
+"id": "bukvich0025"
+
+},{  
+"title": "Flourish","date": "2022","creator": "Bukvich, Daniel","description": "Wind ensemble","location": "Moscow, ID", 
+"id": "bukvich0026"
+
+},{  
+"title": "Three Brazillian Dances","date": "2021","creator": "Bukvich, Daniel","description": "Flute and jazz orchestra","location": "Moscow, ID", 
+"id": "bukvich0027"
+
+},{  
+"title": "Warhorse Mysticanza","date": "2019","creator": "Bukvich, Daniel","description": "Saxophone duo and piano","location": "Moscow, ID", 
+"id": "bukvich0028"
+
+}];
